5ce36ed55df0f0b588122032e749c086da22c66a
Ian Chen <ian@Ians-MacBook-Pro.local>
2019-04-08 15:27:45 +0800

    Fix

--- main.py
+++ main.py
@@ -74,32 +74,14 @@ async def homepage(request):
     else:
         qr = {
                 "type": "text",
-                "text": "Select your favorite food category or send me your location!",
+                "text": "請傳送位置訊息給我",
                 "quickReply": {
                     "items": [
                         {
                             "type": "action",
-                            "imageUrl": "https://example.com/sushi.png",
-                            "action": {
-                                "type": "message",
-                                "label": "Sushi",
-                                "text": "Sushi"
-                                }
-                            },
-                        {
-                            "type": "action",
-                            "imageUrl": "https://example.com/tempura.png",
-                            "action": {
-                                "type": "message",
-                                "label": "Tempura",
-                                "text": "Tempura"
-                                }
-                            },
-                        {
-                            "type": "action",
                             "action": {
                                 "type": "location",
-                                "label": "Send location"
+                                "label": "傳送位置訊息"
                                 }
                             }
                         ]
